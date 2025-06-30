8c61aea8b1d7a796d9e467393af00c40d4264bd7
buriashok <buri.ashokkumar@gmail.com>
2025-06-30 13:06:54 +0530

    completed variables in javascript

--- 005-Huxen-Dev/app.js
+++ 005-Huxen-Dev/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// console.log("Hello World");
+// console.log({name: "Ashok"});
+// console.warn("This is a Warning");
+// console.table({name:"Ashok", age :"20"});
+// console.clear();
+// console.log("hi this is ashok");
+
+//variables
+//declare 
+let name;
+name ="Ashok";
+// console.log(name); //undefine because we not given any value
+let whatDoYouWannaBecomeInYourLife ="programmer";
+let gender = "male";
+let twitterHandle ="ashok441";
+console.log(`${name}, ${whatDoYouWannaBecomeInYourLife}, ${gender}, ${twitterHandle}`);
+console.log(name,",",whatDoYouWannaBecomeInYourLife,",",gender,",",twitterHandle);
