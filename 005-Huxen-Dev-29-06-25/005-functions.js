8d9fa580ff428aa2e45a4902f83750883771b199
buriashok <buri.ashokkumar@gmail.com>
2025-07-03 23:19:16 +0530

    learned callback functions indetail

--- 005-Huxen-Dev-29-06-25/005-functions.js
+++ 005-Huxen-Dev-29-06-25/005-functions.js
@@ -21,16 +21,48 @@
 // console.log(res);
 
 // Function Declaration
-function product(x, y) {
-    return x * y;
-}
+// function product(x, y) {
+//     return x * y;
+// }
 
-const result = product(20, 30);
-console.log(result);
+// const result = product(20, 30);
+// console.log(result);
 
 // Function Expression
-const greetings = function user(username) {
-    console.log(`Hello ${username}`);
+// const greetings = function user(username) {
+//     console.log(`Hello ${username}`);
+// }
+
+// greetings("Buri Ashok Kumar");
+
+// callBack Functions
+// when we provide a function as an argument to other function
+// that function is known as callback function.
+
+//Example - 1
+// function showCallFun(fn) {
+//     let value = 10;
+//     fn(value);
+// }
+
+// showCallFun( function (value){
+//     console.log(value);
+// })
+
+//Example - 2
+function greet(name, cb) {
+    console.log(`Hello ${name}`);
+    cb();
 }
 
-greetings("Buri Ashok Kumar");
+// function cb() {
+//     console.log("I am callback fuction.");
+// }
+
+// greet("Buri Ashok Kumar", cb);
+
+//or 
+
+greet("Buri Ashok Kumar", function cb() {
+    console.log("I am callback fuction.");
+});
