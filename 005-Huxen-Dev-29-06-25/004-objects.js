3f8464a2f672589eac786241662c27910667db9d
buriashok <buri.ashokkumar@gmail.com>
2025-07-03 18:29:40 +0530

    Learned objects basics

---
+++ 005-Huxen-Dev-29-06-25/004-objects.js
@@ -0,0 +1,43 @@
+// const person = {
+//     firstName : "Ashok Kumar",
+//     lastName : "Buri",
+//     age : "20",
+//     location : [ "planet", "earth" ],
+//     isProgrammer : true
+
+// };
+
+// console.log(typeof person);
+// console.log(person);
+
+//Accessing items from objects
+// console.log(person.loction);
+// console.log(person["location"]);
+
+// const numbers = {
+//     10 : "ten"
+// };
+
+// console.log(numbers[10]);
+
+// const items = {
+//     shampoo : 10
+// };
+// items.toothPaste = 20;
+// items.bottle = 5;
+// delete items.toothPaste;
+// console.log(items);
+
+//task
+
+const car = {
+    type : "fait",
+    model : "500",
+    color : "white"
+};
+
+// console.log(car.type);
+// console.log(car.type = "toyota");
+// console.log(car);
+car.wheels = 4;
+console.log(car);
