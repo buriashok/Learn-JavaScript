2214ca1a4f4c78c508fc87712c50b1d6522e3a01
buriashok <buri.ashokkumar@gmail.com>
2025-07-05 22:34:16 +0530

    Learned Template literals or template strings

---
+++ 005-Huxen-Dev-29-06-25/010-templateLiterals.js
@@ -0,0 +1,10 @@
+const firstName = "Buri";
+const lastName = "Ashok Kumar";
+
+console.log(`${firstName} ${lastName}`);
+
+function info() {
+    return `My name is huxn webdev & I'm 19 yeras old.`;
+}
+console.log(`${info}`);
+console.log(`${info()}`);
