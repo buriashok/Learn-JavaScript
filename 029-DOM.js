c61a2ff306a4b0a862fcbda64d8453c1b339adc0
buriashok <buri.ashokkumar@gmail.com>
2025-07-12 21:33:49 +0530

    learned Accessing Dom elements  and what is dom

---
+++ 029-DOM.js
@@ -0,0 +1,10 @@
+/**
+ * DOM → Document Object Model
+ * 
+ */
+// console.log(document);
+// console.dir(document);
+// console.log(document.head);
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.documentURI);
---
+++ 031-accessingElements.js
@@ -0,0 +1,30 @@
+/**
+ * DOM Selectors
+ * 1.getElementsByTagName
+ * 2.getElementById
+ * 3.getElementsByClassName
+ * 4.querySelector
+ * 5.querySelectorAll
+ *
+ * In css
+ * 1.h1{}
+ * 2.#nameID{}
+ * 3..className{}
+ */
+
+// Tag Name
+// console.log(document.getElementsByTagName("h1"));
+// console.log(document.getElementsByTagName("h1").length);
+
+
+// through ID
+// console.log(document.getElementById("main"));
+
+// Through class name
+// console.log(document.getElementsByClassName("cls"));
+
+// querySelector
+// console.log(document.querySelector(".cls"));
+
+//querySelectorAll
+console.log(document.querySelectorAll(".cls"));
