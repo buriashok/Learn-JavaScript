48e79c13f2d19fd8538ffe9cd0655e161783c0e1
buriashok <buri.ashokkumar@gmail.com>
2025-07-13 09:34:55 +0530

    learned  upto classList

---
+++ 035-classList.js
@@ -0,0 +1,10 @@
+// classList
+const h1 = document.querySelector("h1");
+// console.log(h1.classList);
+// h1.classList.add("styles");
+// console.log(h1.classList);
+
+// 8 : 33 :19 
+// h1.classList.remove("styles");
+
+h1.classList.toggle("styles");
