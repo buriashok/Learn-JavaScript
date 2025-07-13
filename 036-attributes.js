731d04c5fbcd85e714d12bf43fe7a6aa97bdbce2
buriashok <buri.ashokkumar@gmail.com>
2025-07-13 19:09:15 +0530

    learned siblings

---
+++ 036-attributes.js
@@ -0,0 +1,17 @@
+// href
+// const a = document.querySelector("a");
+// console.log((a.href = "www.youtube.com"));
+const input = document.querySelector("input");
+// console.log(input.value);
+// console.log(input.type);
+
+// a.href = "www.youtube.com";
+// console.log((input.placeholder = "Please provide password here."));
+
+// getAttribute(attributeName)
+// console.log(input.getAttribute("type"));
+// console.log(input.getAttribute("placeholder"));
+
+// setAttribute(attributeName, value)
+input.setAttribute("placeholder", "buri");
+input.setAttribute("type", "password");
